3d24b2de970d7ed259bf0aa8e4b1842e3a1a5589
Olivier <39884051+ollprogram@users.noreply.github.com>
2021-07-29 20:57:00 +0200

    Version 1.1.0 exemple
    Exemple for the version 1.1.0

--- ImageModifier/tests/exemple/Example.java
+++ ImageModifier/tests/exemple/Example.java
@@ -4,19 +4,17 @@ import fr.olleroy.ImageModifier.BMPFile;
 import fr.olleroy.ImageModifier.components.Bitmap;
 import fr.olleroy.ImageModifier.components.Pixel;
 
+import java.awt.Color;
 import java.io.IOException;
 
 class Example {
     public static void main(String[] args) throws IOException {
         imageCreationTest();
-        loadingTest();
+        duplicateFilesTest();
         fromURLTest();//Be sure to have an internet connection
         monochrome();
     }
-    
-    /**
-    * Image creation example
-    */
+
     private static void imageCreationTest() throws IOException {
         Bitmap bmp = new Bitmap(2, 2);
         bmp.setPixel(0,0, new Pixel(122, 0, 255, 255));
@@ -27,26 +25,20 @@ class Example {
         file.save("there.bmp", true);
     }
 
-    /**
-    * Loading and saving image example
-    */
-    private static void loadingTest() throws IOException {
-        BMPFile file = new BMPFile("there.bmp");
-        file.save("saved.bmp", true);
+    private static void duplicateFilesTest() throws IOException {
+        BMPFile file = new BMPFile("there.bmp");//loading from a pathname
+        BMPFile f2 = new BMPFile(file.getBitmap());//loading from a Bitmap (duplicating the file)
+        f2.getBitmap().setPixel(0,0, Color.MAGENTA);
+        file.save("saved1.bmp", false);
+        f2.save("saved2.bmp", false);
     }
-    
-    /**
-    * Downloading an image from an URL
-    */
+
     private static void fromURLTest() throws IOException {
         BMPFile f = new BMPFile("https://p4.wallpaperbetter.com/wallpaper"
                 +"/368/353/216/animal-cute-meerkat-wallpaper-preview.jpg");
         f.save("fromUrl.bmp", false);
     }
 
-    /**
-    * Modifying an image 
-    */
     private static void monochrome() throws IOException {
         BMPFile f = new BMPFile("fromUrl.bmp");
         Bitmap bmp = f.getBitmap();
